0b798550144d3342446c1e8914a490114c8db116
Ctrl-Zack <zakyzein2006@gmail.com>
2025-10-23 13:42:40 +0700

    seosijfsjdofsdfsdfsd

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>{{ $title ?? 'Library' }}</title>
-    <link rel="stylesheet" href="public\build\assets\app-BtzhhjKA.css">
+    <script src="https://cdn.tailwindcss.com"></script>
 </head>
 <body class="pt-16">
     @include('layouts.navbar')
